2dd8677331e9f842e25cb6b0e56c341e20696a76
bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-05-08 18:27:14 +0530

    dotfiles arch

--- nvim/.config/nvim/lua/bhuvnesh/lazy/colors.lua
+++ nvim/.config/nvim/lua/bhuvnesh/lazy/colors.lua
@@ -5,8 +5,8 @@ function ColorMyPencils(color)
     -- color = color or "kanagawa"
     -- color = color or "gruvbuddy"
     vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 return {
---
+++ nvim/.config/nvim/lua/bhuvnesh/lazy/wakatime.lua
@@ -0,0 +1,7 @@
+return {
+  'wakatime/vim-wakatime',
+  lazy = false,   -- Load on startup
+  config = function()
+  end
+}
+
