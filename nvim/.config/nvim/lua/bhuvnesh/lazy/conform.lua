420419fb26ae143e647716f145f69e9ddb586ba5
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-05-24 06:16:00 +0530

    conform_jsx

--- nvim/.config/nvim/lua/bhuvnesh/lazy/conform.lua
+++ nvim/.config/nvim/lua/bhuvnesh/lazy/conform.lua
@@ -1,3 +1,21 @@
+-- return {
+-- 	"stevearc/conform.nvim",
+-- 	opts = {},
+-- 	config = function()
+-- 		require("conform").setup({
+-- 			formatters_by_ft = {
+-- 				lua = { "stylua" },
+-- 				go = { "goimports", "gofmt" },
+-- 				python = { "black" },
+-- 				typescript = { { "prettierd", "prettier" } },
+-- 				typescriptreact = { { "prettierd", "prettier" } },
+-- 				javascript = { { "prettierd", "prettier" } },
+-- 				javascriptreact = { { "prettierd", "prettier" } },
+-- 			},
+-- 		})
+-- 	end,
+-- }
+
 return {
 	"stevearc/conform.nvim",
 	opts = {},
@@ -5,9 +23,33 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				go = { "gofmt" },
+				go = { "goimports", "gofmt" },
 				python = { "black" },
+				typescript = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				javascript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
 			},
+			-- Use the first available formatter in the list
+			format_on_save = false,
+			formatters = {
+				prettierd = {
+					prepend_args = { "--tab-width", "4" },
+				},
+				prettier = {
+					prepend_args = { "--tab-width", "4" },
+				},
+			},
+			-- Set global default to stop after the first available formatter
+			format_after_save = {
+				lsp_fallback = true,
+			},
+			-- This is the new way to mimic `{ { "a", "b" } }` behavior:
+			-- conform will pick the first available
+			-- formatter in the list per filetype and stop
+			-- if one succeeds.
+			-- Global setting:
+			stop_after_first = true,
 		})
 	end,
 }
