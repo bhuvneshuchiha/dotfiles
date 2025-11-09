f26a8fa54b09b5a7170c0356b1d4ff12548af07f
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-11-09 15:25:34 +0530

    colorizer

--- nvim/.config/nvim/lua/bhuvnesh/init.lua
+++ nvim/.config/nvim/lua/bhuvnesh/init.lua
@@ -144,7 +144,7 @@ vim.opt.splitbelow = true
 --     end,
 -- })
 
--- IMPORTANT **RestoreCursorPosition**
+-- IMPORTANT **RestoreCursorPosition** -- OLDEST
 -- function RestoreCursorPosition()
 --     local line = vim.fn.line("'\"")
 --     if
@@ -163,16 +163,31 @@ vim.opt.splitbelow = true
 --   ]])
 -- end
 
--- Another way to restore cursor position
+-- Another way to restore cursor position -- OLD ONE
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+-- 	callback = function(args)
+-- 		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+-- 		local line_count = vim.api.nvim_buf_line_count(args.buf)
+-- 		if mark[1] > 0 and mark[1] <= line_count then
+-- 			vim.cmd('normal! g`"zz')
+-- 		end
+-- 	end,
+-- })
+
+-- BRAND NEW
 vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function(args)
 		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
 		local line_count = vim.api.nvim_buf_line_count(args.buf)
 		if mark[1] > 0 and mark[1] <= line_count then
-			vim.cmd('normal! g`"zz')
+			vim.api.nvim_win_set_cursor(0, mark)
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
 		end
 	end,
 })
+
 
 -- Set Netrw file menu to bold
 vim.api.nvim_create_autocmd("FileType", {
