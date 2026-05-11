4b6c5cc085fe59cc8e957255d847582496161869
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2026-05-12 01:31:55 +0530

    fix: trouble

--- nvim/.config/nvim/lua/bhuvnesh/lazy/trouble.lua
+++ nvim/.config/nvim/lua/bhuvnesh/lazy/trouble.lua
@@ -4,7 +4,7 @@ return {
             require("trouble").setup({ })
 
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
+                require("trouble").toggle("diagnostics")
             end)
 
             vim.keymap.set("n", "[t", function()
